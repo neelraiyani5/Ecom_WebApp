805183c7cf722f375097060482721965d9666a25
neelraiyani5 <125970410+neelraiyani5@users.noreply.github.com>
2025-07-18 10:57:52 +0530

    configured vite file

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,12 +7,12 @@ export default defineConfig({
   server: {
     proxy : {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://nextcart-qcqi.onrender.com",
         changeOrigin: true,
         secure: false,
       },
       "/user": {
-        target: "http://localhost:8080",
+        target: "https://nextcart-qcqi.onrender.com",
         changeOrigin: true,
         secure: false,
       }
